876d73c48e41b60a2d3f832097d91862ef1225d4
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-13 12:54:44 -0400

    Delete comment

--- spider.java
+++ spider.java
@@ -2,10 +2,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class spider extends Actor
 {
-    /**
-     * Act - do whatever the car wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         move(-4);
