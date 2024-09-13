dc41c624ddd94141ce2d05b55f9b10406f12ff5e
khollyli <94080024+khollyli@users.noreply.github.com>
2024-09-13 13:27:10 -0400

    Move spider

--- spider.java
+++ spider.java
@@ -1,19 +1,25 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class spider here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class spider extends Actor
 {
-    /**
-     * Act - do whatever the spider wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
-        // Add your action code here.
+        move(-4);
+        
+        if(getX() <= 0)
+        {
+            resetSpider();
+        }
+    }
+    
+    public void resetSpider()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
